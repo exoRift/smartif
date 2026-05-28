5b0a7c097bbca42d8ca181a3fa5b30a51c70db74
Arthur Neuman <arthurbneuman@gmail.com>
2026-05-28 11:55:16 -0400

    Add building scaffold

--- index.ts
+++ index.ts
@@ -1,24 +1,55 @@
-interface Else<Return, LastNext> {
-  (this: Block<Return, LastNext>, then: (value: LastNext | undefined) => Return): Omit<Block<Exclude<Return, undefined>, LastNext>, 'else'>
-  if: Block<Return, LastNext>['if']
+interface Else<Return, Forfeiture> {
+  /**
+   * Perform an "else" operation
+   * @param then The "then" clause
+   * @returns    The logical block (with only .unwrap() available)
+   */
+  (this: Block<Return, Forfeiture>, then: (value: Forfeiture | undefined) => Return): Omit<Block<Exclude<Return, undefined>, Forfeiture>, 'else'>
+  /**
+   * Perform an "else if" operation
+   * @param
+   */
+  if: Block<Return, Forfeiture>['if']
 }
 
-class Next {}
+/**
+ * A "Next" operation, telling the block to move onto the next condition step in the chain
+ */
+class Next {} // eslint-disable-line @typescript-eslint/no-extraneous-class
 
+/**
+ * A "Forfeit" operation, telling the block to give up on the chain and jump to the "else"
+ */
 class Forfeit<T> {
+  /** The forfeit value */
   value: T
 
+  /**
+   * Construct a forfeiture
+   */
   constructor (value: T) {
     this.value = value
   }
 }
 
 interface Proceed<T> {
+  /**
+   * Return me in a condition clause to skip to the next "else if" clause
+   * @returns The operation
+   */
   next: () => Next
+  /**
+   * Return me in a condition clause to skip straight to the "else" clause
+   * @param value The value to pass to the else "then" clause
+   * @returns     The operation
+   */
   forfeit: (value: T) => Forfeit<T>
 }
 
 // TODO: if.lazy
+/**
+ * A smart if condition block (chain of conditions)
+ */
 class Block<Return, Forfeiture> {
   protected readonly steps: Array<[any, (value: any, proceed: Proceed<any>) => any]>
   protected fallback?: (value: Forfeiture | undefined) => Return
@@ -34,11 +65,16 @@ class Block<Return, Forfeiture> {
 
   else: Else<Return, Forfeiture>
 
-  // The types of condition and value must match but this is a private class so we don't care about constraining the constructor
+  /**
+   * Construct a condition block
+   * @note The types of condition and value must match but this is a private class so we don't care about constraining the constructor
+   * @param condition The first condition
+   * @param then      The first "then" clause
+   */
   constructor (condition: any, then: (value: any, proceed: Proceed<any>) => Return | Next | Forfeit<any>) {
     this.proceed = {
-      next: this.next,
-      forfeit: this.forfeit
+      next: this.next.bind(this),
+      forfeit: this.forfeit.bind(this)
     }
 
     this.steps = [[condition, then]]
@@ -47,32 +83,50 @@ class Block<Return, Forfeiture> {
     this.fulfilled = false
     this.forfeited = false
 
-    this.else = function (then) {
-      this.fallback = then
+    this.else = function (elsethen) {
+      this.fallback = elsethen
       this.evaluate()
       return this as any
     } as Else<Return, Forfeiture>
     this.else = this.else.bind(this) as Else<Return, Forfeiture>
-    this.else.if = this.if.bind(this) as Else<Return, Forfeiture>['if']
+    this.else.if = this.if.bind(this)
 
     this.evaluate()
   }
 
+  /**
+   * Perform an "if" statement (this is protected because this if will always be called as a .else.if)
+   * @param condition The condition to evaluate
+   * @param then      The "then" clause
+   * @returns         The logical block
+   */
   protected if<T, F, R extends Return | Next | Forfeit<F>> (condition: T, then: (value: T, proceed: Proceed<F>) => R): Block<Return | R, Forfeiture | F> {
     this.steps.push([condition, then])
     this.evaluate()
     return this as any
   }
 
-  protected next () {
+  /**
+   * Return a new "next" operation instance
+   * @returns The operation
+   */
+  protected next (): Next {
     return new Next()
   }
 
-  protected forfeit<F> (value: F) {
+  /**
+   * Return a new "forfeit" operation instance
+   * @param value The value to pass to the else "then" clause
+   * @returns     The operation
+   */
+  protected forfeit<F> (value: F): Forfeit<F> {
     return new Forfeit(value)
   }
 
-  protected evaluate () {
+  /**
+   * Perform the next evaluation steps available in the chain
+   */
+  protected evaluate (): void {
     while (!this.fulfilled && !this.forfeited && this.runIndex < this.steps.length) {
       const step = this.steps[this.runIndex]!
       const condition = step[0]
@@ -100,6 +154,10 @@ class Block<Return, Forfeiture> {
     }
   }
 
+  /**
+   * Return the value returned by the evaluated condition clause
+   * @returns The final resolved value
+   */
   unwrap (): Return {
     if (this.fulfilled) return this.returnValue!
     else return undefined as Return
